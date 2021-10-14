e7fe6aa56006b07ea329fbeecce3fdca98508547
Sebastien Rousseau <sebounoob@gmail.com>
2021-10-14 11:12:42 +0200

    hard redux

--- src/index.jsx
+++ src/index.jsx
@@ -5,14 +5,18 @@ import { Provider } from 'react-redux';
 import { createStore, combineReducers } from 'redux';
 
 // internal modules
-import App from './components/app';
 import '../assets/stylesheets/application.scss';
 
+import ActiveCityReducer from './reducers/active_city_reducer';
+import citiesReducer from './reducers/cities_reducer'
 // State and reducers
 const reducers = combineReducers({
-  changeMe: (state = null, action) => state
+  cities: citiesReducer,
+  activeCity: ActiveCityReducer
 });
 
+import App from './components/app';
+import activeCityReducer from './reducers/active_city_reducer';
 // render an instance of the component in the DOM
 ReactDOM.render(
   <Provider store={createStore(reducers)}>
